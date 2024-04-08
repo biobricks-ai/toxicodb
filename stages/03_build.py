32a517aa4de205c06d26fbec00687eb14c3729a5
bhlieberman <bhlieberman93@gmail.com>
2024-04-08 16:06:12 +0000

    built with new changes on ws2

--- stages/03_build.py
+++ stages/03_build.py
@@ -1,25 +1,21 @@
 import pandas as pd
-import os
-import pathlib
+from pathlib import Path
 
 def entrez_gid_zero(gid):
     if gid == "0":
         return None
     return gid
 
-os.makedirs("brick", exist_ok=True)
+raw_path = Path('raw')
 
-csv_files = pathlib.Path("raw").rglob("*.csv")
+brick_path = Path('brick')
 
-f = list( pathlib.Path('raw').rglob('*/*.csv') )[0]
-f.name.replace('.csv', '.parquet')
-pathlib.Path('brick') / f.relative_to('raw')
-
-for file in csv_files:
-    filename = file.name.replace(".csv", ".parquet")
+for infile in list( raw_path.rglob('*.csv') ):
+    outfile = brick_path / infile.relative_to(raw_path).with_suffix('.parquet')
+    outfile.parent.mkdir(parents=True, exist_ok=True)
     csv = pd.read_csv(
-        file, 
+        infile, 
         keep_default_na=False, 
         converters={"entrez_gid": entrez_gid_zero}
     )
-    csv.to_parquet(filename)
+    csv.to_parquet(outfile)
