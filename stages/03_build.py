450a0f64ff0e9db8e7c01b18c310725fbec2d98d
Ben Lieberman <bhlieberman93@gmail.com>
2024-04-08 09:01:19 -0600

    switched build step from shell script to Python script

--- stages/03_build.py
+++ stages/03_build.py
@@ -1,21 +1,25 @@
-import glob
 import pandas as pd
-
-csv_files = glob.glob("raw/*/*.csv")
-
+import os
+import pathlib
 
 def entrez_gid_zero(gid):
     if gid == "0":
         return None
     return gid
 
+os.makedirs("brick", exist_ok=True)
+
+csv_files = pathlib.Path("raw").rglob("*.csv")
+
+f = list( pathlib.Path('raw').rglob('*/*.csv') )[0]
+f.name.replace('.csv', '.parquet')
+pathlib.Path('brick') / f.relative_to('raw')
 
 for file in csv_files:
-    filename = file.split(".")[0]
-    parquet_name = filename + ".parquet"
+    filename = file.name.replace(".csv", ".parquet")
     csv = pd.read_csv(
         file, 
         keep_default_na=False, 
         converters={"entrez_gid": entrez_gid_zero}
     )
-    csv.to_parquet(parquet_name)
+    csv.to_parquet(filename)
